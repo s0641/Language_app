bf8285e6e9f4de32195436d04ab8972c9a1f2eef
Shubham Gupta( s0641) <48442627+s0641@users.noreply.github.com>
2020-05-06 22:10:09 +0530

    Add files via upload

---
+++ src/androidTest/java/com/app/language/MainActivityTest.groovy
@@ -0,0 +1,4 @@
+package com.app.language
+
+class MainActivityTest {
+}
---
+++ src/main/java/com/app/language/MainActivity.java
@@ -0,0 +1,106 @@
+package com.app.language;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends BaseActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
+  DrawerLayout drawerLayout;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+
+
+    drawerLayout = findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+        R.string.navigation_drawer_close);
+    drawerLayout.addDrawerListener(toggle);
+    toggle.syncState();
+
+    NavigationView navigationView = findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
+  }
+
+  @Override
+  public void onBackPressed() {
+    if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+      drawerLayout.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    switch (id) {
+      case R.id.local_english:
+        setNewLocale(this, LocaleManager.ENGLISH);
+        return true;
+      case R.id.local_hindi:
+        setNewLocale(this, LocaleManager.HINDI);
+        return true;
+
+      case R.id.local_spanish:
+        setNewLocale(this, LocaleManager.SPANISH);
+        return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void setNewLocale(AppCompatActivity mContext, @LocaleManager.LocaleDef String language) {
+    LocaleManager.setNewLocale(this, language);
+    Intent intent = mContext.getIntent();
+    startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+  }
+
+  @SuppressWarnings("StatementWithEmptyBody")
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+    switch (id) {
+      case R.id.nav_camera:
+        break;
+      case R.id.nav_gallery:
+        break;
+      case R.id.nav_slideshow:
+        break;
+      case R.id.nav_manage:
+        break;
+      case R.id.nav_share:
+        break;
+      case R.id.nav_send:
+        break;
+
+      default:
+        break;
+    }
+    drawerLayout.closeDrawer(GravityCompat.START);
+    return true;
+  }
+}
